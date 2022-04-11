a89add4599343dd3e061dac936c2f64c669f0f06
Nish1779 <64070976+Nish1779@users.noreply.github.com>
2022-04-11 15:25:07 -0400

    Initial Commit

---
+++ Wagee_IOS_Project_Semester_One/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wagee_IOS_Project_Semester_One
+//
+//  Created by Nish Varia on 2022-04-11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
