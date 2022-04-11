d656e31c4ee5917be6eadf719e507dc13c192ded
Nish1779 <64070976+Nish1779@users.noreply.github.com>
2022-04-11 15:56:48 -0400

    new

--- Wagee_IOS_Project_Semester_One/ViewController.swift
+++ Wagee_IOS_Project_Semester_One/ViewController.swift
@@ -7,13 +7,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var userNameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        userNameField.delegate = self
+        passwordField.delegate = self
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func employerButton(_ sender: Any) {
+        if(userNameField.text!.isEqual("e") && passwordField.text!.isEqual("1") ){
+            
+        }
+        
+        
+        
+        
+        
+    }
+    
+    
+    
+    @IBAction func employeeButton(_ sender: Any) {
+    }
+    
+    
+    
 }
 
